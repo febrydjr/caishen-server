f005cbc127fcc5b95b32780600052acb5963e418
Febry Dharmawan Junior <83161819+febrydjr@users.noreply.github.com>
2023-08-25 11:33:57 +0700

    get total

--- src/services/cart/getTotal.js
+++ src/services/cart/getTotal.js
@@ -1,3 +1,38 @@
+// const { fn, literal, cast } = require("sequelize");
+// const db = require("../../../models");
+// const { messages } = require("../../helpers");
+
+// const carts = db["cart"];
+// const products = db["product"];
+// const cart_items = db["cart_item"];
+
+// async function checkCart(id_user) {
+//     const result = await carts.findOne({ where: { id_user } });
+//     return result;
+// }
+
+// async function getTotal(id) {
+//     const cart = await checkCart(id);
+//     if (!cart) return messages.error(500, "No cart found");
+
+//     let result = await cart_items.findOne({
+//         attributes: [
+//             "id_cart",
+//             [
+//                 cast(fn("SUM", literal("qty * product.price")), "UNSIGNED"),
+//                 "total",
+//             ],
+//         ],
+//         include: [{ model: products, attributes: [] }],
+//         where: { id_cart: cart["id"] },
+//     });
+
+//     if (!result.dataValues["total"]) result = { id_cart: cart["id"], total: 0 };
+//     return messages.success("", result);
+// }
+
+// module.exports = getTotal;
+
 const { fn, literal, cast } = require("sequelize");
 const db = require("../../../models");
 const { messages } = require("../../helpers");
@@ -15,7 +50,7 @@ async function getTotal(id) {
     const cart = await checkCart(id);
     if (!cart) return messages.error(500, "No cart found");
 
-    let result = await cart_items.findOne({
+    const result = await cart_items.findOne({
         attributes: [
             "id_cart",
             [
@@ -23,12 +58,19 @@ async function getTotal(id) {
                 "total",
             ],
         ],
-        include: [{ model: products, attributes: [] }],
+        include: [
+            { model: products, attributes: [] },
+            // Include this line to group by cart items
+            { model: carts, attributes: [], required: true },
+        ],
         where: { id_cart: cart["id"] },
+        group: ["cart_item.id_cart"], // Group by cart ID
     });
 
-    if (!result.dataValues["total"]) result = { id_cart: cart["id"], total: 0 };
-    return messages.success("", result);
+    // If no rows match the query, return 0 as total
+    const total = result ? result.dataValues["total"] : 0;
+    
+    return messages.success("", { id_cart: cart["id"], total });
 }
 
 module.exports = getTotal;
